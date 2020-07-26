a07b30558ba9373d7bec6017e7a68fad4a838822
Sidak <sidak.pasrichassp@gmail.com>
2020-07-26 21:54:58 +0530

    Build Schema + Repository

---
+++ src/main/java/com/udacity/graphql/entity/Dog.java
@@ -0,0 +1,19 @@
+package com.udacity.graphql.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String name;
+    String breed;
+    String origin;
+
+}
